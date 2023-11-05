2cdae9611c5e7179f3f180526f46000b60fb74c5
Thomas Teguh <thmstgh@gmail.com>
2023-11-05 19:01:49 +0700

    login 1

---
+++ controllers/dbhandler.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func connectGORM() *gorm.DB {
+	dsn := "root:@tcp(127.0.0.1:3306)/ctrl_c?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
--- controllers/model.go
+++ controllers/model.go
@@ -1,1 +1,8 @@
 package controllers
+
+type User struct {
+	ID       int    `json: "id"`
+	Name     string `json: "full_name"`
+	Email    int    `json: "email"`
+	UserType bool   `json: "acc_type"`
+}
