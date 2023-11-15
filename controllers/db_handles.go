876e7a11fcb16b3e50d232cd94a05e5414a5b9b8
Thomas Teguh <thmstgh@gmail.com>
2023-11-15 18:51:52 +0700

    post order

--- controllers/db_handles.go
+++ controllers/db_handles.go
@@ -1,6 +1,12 @@
 package controllers
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -26,6 +32,18 @@ func connect() *gorm.DB {
 	err = sqlDB.Ping()
 	if err != nil {
 		panic("Failed to ping database: " + err.Error())
+	}
+	return db
+}
+
+func connectMux() *sql.DB {
+	_ = godotenv.Load()
+	db_port := os.Getenv("DB_PORT")
+	db_name := os.Getenv("DB_NAME")
+	dbaddress := fmt.Sprintf("root:@tcp(127.0.0.1:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", db_port, db_name)
+	db, err := sql.Open("mysql", dbaddress)
+	if err != nil {
+		log.Fatal()
 	}
 	return db
 }
